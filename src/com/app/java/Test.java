3ed2f0301c3484cad818d3157896bfa049054f3c
Dell <Dell@Dell-PC>
2019-05-09 14:40:50 +0530

    test

---
+++ src/com/app/java/Test.java
@@ -0,0 +1,19 @@
+package com.app.java;
+
+public class Test {
+
+	public void m1() {
+		System.out.println("vaagdevi Engineering college");
+	}
+
+	public void m2() {
+		m1();
+		System.out.println("sr engineerig college");
+	}
+
+	public static void main(String[] args) {
+
+		Test t = new Test();
+		t.m2();
+	}
+}
