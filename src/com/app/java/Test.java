30bcc027bdd25aa891d7378c87ab8359f1022e33
Dell <Dell@Dell-PC>
2019-05-09 14:57:57 +0530

    Test1

--- src/com/app/java/Test.java
+++ src/com/app/java/Test.java
@@ -11,6 +11,10 @@ public class Test {
 		System.out.println("sr engineerig college");
 	}
 
+	public void m3() {
+		System.out.println("jayamukhi eng college");
+	}
+
 	public static void main(String[] args) {
 
 		Test t = new Test();
